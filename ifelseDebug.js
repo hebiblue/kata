3b2f05e3af698e5e52c7c0df5757bf1bd5f2f628
Alexia <alexiatgh@gmail.com>
2022-02-02 16:56:57 +0100

    If/Else syntax debug lvl 8

---
+++ ifelseDebug.js
@@ -0,0 +1,11 @@
+// If/Else syntax debug kata level 8
+// kata's link: https://www.codewars.com/kata/57089707fe2d01529f00024a
+
+function checkAlive(health){
+    if(health > 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
